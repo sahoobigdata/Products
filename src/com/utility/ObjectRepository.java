72fa4a53489f80d696c23121d546c5c72b36fd2d
sahoobigdata <sahoobigdata@gmail.com>
2015-05-10 01:01:12 -0400

    Revert "Revert "2nd commit""
    This reverts commit 06a7bf149fae9e3e9cec76eb3c8c047c004a5996.

---
+++ src/com/utility/ObjectRepository.java
@@ -0,0 +1,9 @@
+package com.utility;
+
+public class ObjectRepository {
+	
+	public static String BRANDNAME = "//*[@id='brand']";
+	public static String SEARCHBUTTON="//*[@id='search_button']";
+	public static String DEFINITION="html/body/div[3]/a";
+
+}
